23529b419670df6da6db6eb912e18ae9508cb918
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2013-04-08 09:46:57 +0300

    First compiling version

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(piqi):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="piqi" command="/usr/bin/piqi"
--- debian/piqi.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the piqi package
-#
-0 4	* * *	root	[ -x /usr/bin/piqi_maintenance ] && /usr/bin/piqi_maintenance
