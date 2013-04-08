ec26febed063fd5379050febb3dd75b3db690a54
Motiejus Jakštys <motiejus.jakstys@spilgames.com>
2013-04-08 09:32:29 +0300

    dh_make templates

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(piqi):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="piqi" command="/usr/bin/piqi"
---
+++ debian/piqi.default.ex
@@ -0,0 +1,10 @@
+# Defaults for piqi initscript
+# sourced by /etc/init.d/piqi
+# installed at /etc/default/piqi by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
